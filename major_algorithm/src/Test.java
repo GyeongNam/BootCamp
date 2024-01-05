6011d910aa04798cf950d603eeeab541e72ed907
GyeongNam <jo_gn97@naver.com>
2024-01-05 18:19:55 +0900

    html css js 4일차

--- major_algorithm/src/Test.java
+++ major_algorithm/src/Test.java
@@ -3,65 +3,42 @@ import java.util.*;
 
 public class Test {
     public static void main(String[] args) throws IOException {
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        int a = Integer.parseInt(br.readLine());
-        int i, j = 1;
-        for (i = 1; i < a; i++) {
-            if (a == 1) {
-                System.out.println(i);
-            }
-            a -= i;
-        }
-        if ((i % 2) == 0) {
-            a--;
-            while (a != 0) {
-                i--;
-                j++;
-                a--;
-            }
-            System.out.println(j+ "/" + i);
-        }
-        else {
-            a = i - a;
-            while (a != 0) {
-                i--;
-                j++;
-                a--;
-            }
-            System.out.println(j+ "/" + i);
-        }
+//        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+//        int a = Integer.parseInt(br.readLine());
+//        int i, j = 1;
+//        for (i = 1; i < a; i++) {
+//            if (a == 1) {
+//                System.out.println(i);
+//            }
+//            a -= i;
+//        }
+//        if ((i % 2) == 0) {
+//            a--;
+//            while (a != 0) {
+//                i--;
+//                j++;
+//                a--;
+//            }
+//            System.out.println(j+ "/" + i);
+//        }
+//        else {
+//            a = i - a;
+//            while (a != 0) {
+//                i--;
+//                j++;
+//                a--;
+//            }
+//            System.out.println(j+ "/" + i);
+//        }
 
-//        solution("hit","cog", new String[] {"hot", "dot", "dog", "lot", "log", "cog"});
-//        solution("hit","cog", new String[] {"hot", "dot", "dog", "lot", "log"});
+        solution(new int[] {2, 3, 3, 5});
+        solution(new int[] {9, 1, 5, 3, 6, 2});
 
     }
 
-    public static long solution(String begin, String target, String[] words) {
-        int answer = 0;
-        boolean[] visited = new boolean[words.length];
-        Queue<String> q = new LinkedList<>(); // 변환 가능한 단어들을 저장할 queue
-        Set<String> set = new HashSet<>(Arrays.asList(words));
-        if(!set.contains(target)){
-            return 0;
-        }
-        q.offer(begin);
-        set.remove(begin);
-        while (!q.isEmpty()){
-            for (int i=0; i<q.size(); i++){
-                String temp = q.poll();
-                if (temp.equals(target)){
-                    return answer; // 타겟 단어에 도달
-                }
-                for (String word : set.toArray(new String[set.size()])){
-                    if(convert(temp,word)){
-                        q.offer(word);
-                        set.remove(word);
-                    }
-                }
-            }
-            answer++;
-        }
-        return 0;
+    public static int[] solution(int[] numbers) {
+        int[] answer = new int[numbers.length];
+        return answer;
     }
     static private boolean convert(String word1, String word2){
         int diffCnt = 0;
